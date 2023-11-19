ddd61d5b1d8b0e844bdf5d74be394cab9037ac40
Kate <kate.bade@student.ie.edu>
2023-11-19 19:36:32 +0100

    Initial commit for OpenAI API project

---
+++ Intro.py
@@ -0,0 +1,24 @@
+# Installations and imports
+#pip install streamlit
+#pip install python-dateutil
+#pip install "pyzmq==17.0.0" "ipykernel==4.8.2"
+
+import streamlit as st
+
+# Sidebar
+st.sidebar.success("Select a page above.")
+
+# Main Content
+st.title('AI and Big Data in Business Strategy')
+st.header('A Course for the Future')
+
+st.markdown(
+    """
+    Welcome to AI and Big Data in Business Strategy! This course is an AI-driven experience. Throughout this course 
+    you will dive deeply into artificial intelligence and machine learning. Together, we will learn about the value 
+    proposition of AI, how to put AI into operation in businesses, the challenges of implementing AI, and much more. 
+    The best way to learn about a topic is to immerse yourself in it. Therefore, to learn about AI, you will be assisted 
+    by your new best friend, Alex, a machine learning bot. Alex knows just about everything (except, perhaps, the 
+    meaning of life.) Click on to the next page to begin chatting with him.
+    """
+)
---
+++ pages/Coursework.py
@@ -0,0 +1,36 @@
+#pip install streamlit
+#pip install python-dateutil
+#pip install "pyzmq==17.0.0" "ipykernel==4.8.2"
+
+import streamlit as st
+import openai
+import os
+
+st.sidebar.success("Select a page above.")
+
+st.title('AI and Big Data in Business Strategy')
+st.header('A Course for the Future')
+
+st.markdown(
+    """
+    Welcome to the Coursework page. Here is where you can talk with Alex about all things artificial intelligence. 
+    Click into the text box below to begin learning!
+    """
+)
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+user_input = st.text_input('What is your query?')
+
+if st.button('Submit here.'):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=user_input,
+        temperature=0.8,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    st.write('As a reminder, your query was:', user_input)
+    st.write("Alex's response:", response.choices[0].text)
